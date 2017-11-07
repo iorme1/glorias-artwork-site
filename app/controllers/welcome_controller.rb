dc51de9fd149a9aeca93e9eeca896bc7174188fc
Isaac Orme <isorme1@gmail.com>
2017-11-07 14:04:33 -0800

    heroku-fixes-plus-welcome-controller-fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @images = Image.all
-    @image_0 = @images[-5].image_url
-    @image_1 = @images[-4].image_url
-    @image_2 = @images[-3].image_url
-    @image_3 = @images[-2].image_url
-    @image_4 = @images[-1].image_url
+    if @images[-5] && @images[-4] && @images[-3] && @images[-2] && @images[-1]
+      @image_0 = @images[-5].image_url
+      @image_1 = @images[-4].image_url
+      @image_2 = @images[-3].image_url
+      @image_3 = @images[-2].image_url
+      @image_4 = @images[-1].image_url
+    end
   end
 
   def show
